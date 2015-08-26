370a9b875aaf2e80c848545d076ef5a7d096d583
Samantha Degges <samanthadegges@gmail.com>
2015-08-25 19:52:06 -0700

    initial commit. Splits parameter, Num, to a String and defines a for loop.

---
+++ happynum.js
@@ -0,0 +1,23 @@
+// homework:
+//sum of the sq of its digits.
+
+var isHappy = function(Num) {
+  var NumToString = Num.toString();
+
+  for (var i = 0; i < NumToString.length; i++) {
+    //var lengthOfNumString = stringOfNum.length;
+    console.log('i is:' + i);
+
+    //access each digit using I because NumToString is not a global variable.
+
+  }
+
+
+  //sq each
+  //add
+  //when equal 1; return happy number
+  //var x = Math.pow(digit,2);
+  //console.log(digit, x);
+};
+
+isHappy(17);
