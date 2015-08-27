631ffb38b26a3dce98ba55b2351cffc068cd1e73
Samantha Degges <samanthadegges@gmail.com>
2015-08-27 15:25:10 -0700

    html not centered container.

--- gameExerciseTreasureChest.js
+++ gameExerciseTreasureChest.js
@@ -1,46 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Treasure Game by Samantha</title>
-
-    <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-	<!--My CSS file with customization-->
-	<link rel="stylesheet" type="text/css" href="treasureGame.css">
-
-	<!--font awesome-->
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div id="gameBoard">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
-	 <!-- jQuery (necessary for Bootstrap's JavaScript plugins)-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-    <!-- My JavaScript file that enables click functionality-->
-	<script src="Treasuregame.js"></script>
-
-  </body>
-</html>
+$(document).ready(function() {
+  $("#gameBoard").find("div").on("click", function() {
+    $(this).addClass("red");
+  });
+})
