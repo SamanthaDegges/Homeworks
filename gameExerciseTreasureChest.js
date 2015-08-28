46e57c8cc163a773c950cdd480fd2e3d2ced985c
Samantha Degges <samanthadegges@gmail.com>
2015-08-27 21:43:32 -0700

    call back function started.

--- gameExerciseTreasureChest.js
+++ gameExerciseTreasureChest.js
@@ -1,23 +1,22 @@
 //event handlers
 $(document).ready(function() {
-  var guesses = 0;
+  var guesses = 3;
 
   $('.tile').on('click', function(event) {
-    //initiate count
-
-  //return turns and if turns = 3 then game lost.
-  //if turns = !== 3 and game not won, then continue game.
     $(this).addClass('red');
-    console.log(turnsRemaining());
+    $('strong').text(--guesses);
+
+  if (guesses === 0) {
+    $('#title').text("Game Over.");
+  } else {
+    console.log("Game Running.");
+  }
+    //if turns = !== 3 and game not won, then continue game.
   });
 
 
   var win = function() {
     $(this).addClass('green');
-  }
-
-  var turnsRemaining = function(){ //do i need event or to stop propogate here?
-    return guesses = guesses + 1;
   }
 
 })
