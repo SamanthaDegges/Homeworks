1399c18f3a3e81fc0bb4d69168ddb178986b1ed5
Samantha Degges <samanthadegges@gmail.com>
2015-08-27 22:26:07 -0700

    winning tile turns green. Game does not stop.

--- gameExerciseTreasureChest.js
+++ gameExerciseTreasureChest.js
@@ -1,22 +1,27 @@
 //event handlers
 $(document).ready(function() {
   var guesses = 3;
+  var winningTile = Math.floor((Math.random() * 9) + 1);
+  console.log(winningTile);
 
   $('.tile').on('click', function(event) {
-    $(this).addClass('red');
+    var $this = $(this).addClass('red');
     $('strong').text(--guesses);
 
-  if (guesses === 0) {
-    $('#title').text("Game Over.");
-  } else {
-    console.log("Game Running.");
-  }
+    winCheck($this);
+
+    if (guesses === 0) {
+      $('#title').text("Game Over.");
+    } else {
+      console.log("Game Running.");
+    }
     //if turns = !== 3 and game not won, then continue game.
   });
 
-
-  var win = function() {
-    $(this).addClass('green');
+  var winCheck = function($TileClicked) {
+    if ($TileClicked.data("id") === winningTile) {
+      guesses === 0;
+      $TileClicked.addClass('green');
+    }
   }
-
 })
