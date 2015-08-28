743b456d867bdbe332c782300bee69c8370fac46
Samantha Degges <samanthadegges@gmail.com>
2015-08-27 21:19:03 -0700

    guesses are counted and returned.

--- gameExerciseTreasureChest.js
+++ gameExerciseTreasureChest.js
@@ -1,6 +1,23 @@
 //event handlers
 $(document).ready(function() {
-  $(".tile").on("click", function(event) {
-    $(this).addClass("red");
+  var guesses = 0;
+
+  $('.tile').on('click', function(event) {
+    //initiate count
+
+  //return turns and if turns = 3 then game lost.
+  //if turns = !== 3 and game not won, then continue game.
+    $(this).addClass('red');
+    console.log(turnsRemaining());
   });
+
+
+  var win = function() {
+    $(this).addClass('green');
+  }
+
+  var turnsRemaining = function(){ //do i need event or to stop propogate here?
+    return guesses = guesses + 1;
+  }
+
 })
